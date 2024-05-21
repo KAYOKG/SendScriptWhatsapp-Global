5fc7479bf4af79a4b9d120a58c37adbdd6f520eb
KAYOKG <kayo@venidici.com.br>
2024-05-21 12:29:38 -0300

    up

--- LGPD-SendScript.js
+++ LGPD-SendScript.js
@@ -1,3 +1,28 @@
+async function enviarScript(scriptText) {
+  const lines = scriptText.split(/[\n\t]+/).map(line => line.trim()).filter(line => line);
+  main = document.querySelector("#main"),
+    textarea = main.querySelector(`div[contenteditable="true"]`)
+
+  if (!textarea) throw new Error("Não há uma conversa aberta")
+
+  for (const line of lines) {
+    console.log(line)
+
+    textarea.focus();
+    document.execCommand('insertText', false, line);
+    textarea.dispatchEvent(new Event('change', { bubbles: true }));
+
+    setTimeout(() => {
+      (main.querySelector(`[data-testid="send"]`) || main.querySelector(`[data-icon="send"]`)).click();
+    }, 100);
+
+    if (lines.indexOf(line) !== lines.length - 1) await new Promise(resolve => setTimeout(resolve, 250));
+  }
+
+  return lines.length;
+}
+
+enviarScript(`
 ***************************************
 NÃO AUTORIZO O USO DOS MEUS DADOS
 RESPEITE A LGPD
@@ -1385,3 +1410,4 @@ Raul Jungmann
 Eliseu Padilha
 
 Este texto não substitui o publicado no DOU de 15.8.2018, e republicado parcialmente em 15.8.2018 - Edição extra
+`).then(e => console.log(`Código finalizado, ${e} mensagens enviadas`)).catch(console.error)
